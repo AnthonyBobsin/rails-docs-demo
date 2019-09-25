0b392abbe940fd30e9a0123f46d8b41a359acebf
Anthony Bobsin <bobsinj@gmail.com>
2019-09-24 22:55:05 -0400

    apipie properties for pets controller and json swagger file

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,11 @@
 class Owner < ApplicationRecord
+  class << self
+    def describe_own_properties
+      [
+        Apipie::prop(:name, 'string', description: 'Name of owner'),
+        Apipie::prop(:age, 'string', description: 'Age of owner', required: false),
+        Apipie::prop(:gender, 'string', description: 'Gender of owner', required: false),
+      ]
+    end
+  end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,12 @@
 class Pet < ApplicationRecord
+  class << self
+    def describe_own_properties
+      [
+        Apipie::prop(:name, 'string', description: 'Name of pet'),
+        Apipie::prop(:photo_url, 'string', description: 'Photo of pet', required: false),
+        Apipie::prop(:status, 'string', description: 'Status of pet'),
+        Apipie::prop(:owner, 'Owner', description: 'Owner of pet')
+      ]
+    end
+  end
 end
